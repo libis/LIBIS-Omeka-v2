1dbe419bdc33d6ec4735c61793a1735cd60627af
Joris <joris.lambrechts@kuleuven.be>
2019-08-30 10:11:54 +0200

    profile template

--- themes/default/items/show.php
+++ themes/default/items/show.php
@@ -1,94 +1,136 @@
 <?php echo head(array('title' => metadata('item', array('Dublin Core', 'Title')),'bodyclass' => 'item show')); ?>
 <?php $type = metadata('item','item_type_name');?>
-<?php if (metadata('item', 'has files') && $type != 'News'): ?>
-  <section class="item-section general-section">
-      <div class="container-fluid">
-        <div class='row breadcrumbs'>
-          <div class="col-xs-12">
-              <p id="simple-pages-breadcrumbs">
-                <span><a href="<?php echo url('/');?>">Home</a></span>
-                 > <span><a href="<?php echo $type;?>"><?php echo $type;?></a></span>
-                 > <?php echo metadata('item', array('Dublin Core', 'Title')); ?>
-               </p>
-           </div>
-        </div>
-          <div class="row image-row">
-            <!-- The following returns all of the files associated with an item. -->
-            <div id="itemfiles" class="element">
-                <div class="element-text"><?php echo item_image_gallery(array('linkWrapper' => array('wrapper' => null,'class' => 'col-sm-2 col-xs-12 image')),'thumbnail'); ?></div>
-            </div>
-          </div>
-      </div>
-  </section>
-<?php endif; ?>
-<section class="metadata-section general-section">
-    <div id="content" class='container-fluid' role="main" tabindex="-1">
-        <?php if ($type == 'News'): ?>
-          <div class='row breadcrumbs'>
-            <div class="col-xs-12">
-                <p id="simple-pages-breadcrumbs">
-                  <span><a href="<?php echo url('/');?>">Home</a></span>
+<div class="content-wrapper simple-page-section ">
+  <div class="container simple-page-container">
+    <?php if (metadata('item', 'has files') && $type != 'News'): ?>
+    <div class="row">
+        <div class="col-md-12 col-sm-12 page">
+            <div class='row breadcrumbs'>
+              <div class="col-sm-12 col-xs-12">
+                <p id="simple-pages-breadcrumbs"><span>
+                   <a href="<?php echo url('/');?>">Home</a></span>
                    > <span><a href="<?php echo $type;?>"><?php echo $type;?></a></span>
                    > <?php echo metadata('item', array('Dublin Core', 'Title')); ?>
-                 </p>
-             </div>
-          </div>
-        <?php endif; ?>
-        <div class="row content">
-            <?php if (metadata('item', 'has files') && $type == 'News'): ?>
-              <div class="col-sm-3 col-xs-12 page">
-                <div id="itemfiles" class="element">
-                    <div class="element-text"><?php echo item_image_gallery(array('linkWrapper' => array('wrapper' => null,'class' => 'col-sm-2 col-xs-12 image')),'thumbnail'); ?></div>
-                </div>
+                </span></p>
               </div>
-              <div class="col-sm-9 col-xs-12 page">
-            <?php else:?>
-              <div class="col-sm-12 col-xs-12 page">
-            <?php endif; ?>
-                    <?php if ($type != ''): ?>
-                      <!--<h3 class="type-title"><?php echo $type;?></h3>-->
-                    <?php endif; ?>
-                    <h1 class="section-title projecten-title"><span><?php echo metadata('item', array('Dublin Core', 'Title')); ?></span></h1>
-
-                    <?php if ($type != 'News'): ?>
-                        <?php echo all_element_texts('item'); ?>
-
-                        <!-- If the item belongs to a collection, the following creates a link to that collection. -->
-                        <?php if (metadata('item', 'Collection Name')): ?>
-                        <div id="collection" class="element">
-                            <h3><?php echo __('Collection'); ?></h3>
-                            <div class="element-text"><p><?php echo link_to_collection_for_item(); ?></p></div>
-                        </div>
-                        <?php endif; ?>
-
-                        <!-- The following prints a list of all tags associated with the item -->
-                        <?php if (metadata('item', 'has tags')): ?>
-                        <div id="item-tags" class="element">
-                            <h3><?php echo __('Tags'); ?></h3>
-                            <div class="element-text"><?php echo tag_string('item'); ?></div>
-                        </div>
-                        <?php endif;?>
-
-                        <!-- The following prints a citation for this item. -->
-                        <div id="item-citation" class="element">
-                            <h3><?php echo __('Citation'); ?></h3>
-                            <div class="element-text"><?php echo metadata('item', 'citation', array('no_escape' => true)); ?></div>
-                        </div>
-                    <?php else:?>
-                          <p class="date"><?php echo metadata('item', array('Dublin Core', 'Date')); ?></p>
-                          <p class="description"><?php echo metadata('item', array('Dublin Core', 'Description')); ?></p>
-                    <?php endif; ?>
-                </div>
-                <nav>
-                <ul class="item-pagination navigation">
-                    <li id="previous-item" class="previous"><?php echo link_to_previous_item_show("&#8249; Previous"); ?></li>
-                    <li id="next-item" class="next"><?php echo link_to_next_item_show('Next &#8250;'); ?></li>
-                </ul>
-                </nav>
-
+            </div>
         </div>
     </div>
+    <?php endif; ?>
+    <section class="metadata-section general-section">
+      <h1 class="section-title projecten-title"><span><?php echo metadata('item', array('Dublin Core', 'Title')); ?></span></h1>
 
-</section>
+        <div id="content" class='container' role="main" tabindex="-1">
 
+            <div class="row content">
+                  <?php if (metadata('item', 'has files')): ?>
+                    <div class="col-sm-3 col-xs-12 page">
+                      <div id="itemfiles" class="element">
+                        <?php echo item_image_gallery(array(),'thumbnail'); ?>
+                      </div>
+                    </div>
+                  <?php endif; ?>
+                  <div class="col-sm-9 col-xs-12 page" style="border-left:1px solid #eee;">
+                        <?php if ($type != ''): ?>
+                          <!--<h3 class="type-title"><?php echo $type;?></h3>-->
+                        <?php endif; ?>
+
+                        <?php if ($type != 'News'): ?>
+                            <?php $texts =  all_element_texts('item',array('return_type'=>'array'));?>
+                            <?php if (isset($texts['Profile Item Type Metadata']['Job title'])): ?>
+                            <div class="element">
+                                <h3><?php echo 'Job title'; ?></h3>
+                                <div class="element-text"><?php echo implode(', ',$texts['Profile Item Type Metadata']['Job title']); ?></div>
+                            </div>
+                            <?php endif; ?>
+
+                            <?php if (isset($texts['Profile Item Type Metadata']['Function/Expertise'])): ?>
+                            <div class="element">
+                                <h3><?php echo 'Function/Expertise'; ?></h3>
+                                <div class="element-text"><?php echo implode(', ',$texts['Profile Item Type Metadata']['Function/Expertise']); ?></div>
+                            </div>
+                            <?php endif; ?>
+
+                            <?php if (isset($texts['Profile Item Type Metadata']['Documentation'])): ?>
+                            <div class="element">
+                                <h3><?php echo 'Documentation'; ?></h3>
+                                <div class="element-text"><?php echo implode(', ',$texts['Profile Item Type Metadata']['Documentation']); ?></div>
+                            </div>
+                            <?php endif; ?>
+
+                            <?php if (isset($texts['Profile Item Type Metadata']['Bibliography'])): ?>
+                            <div class="element">
+                                <h3><?php echo 'Bibliography'; ?></h3>
+                                <div class="element-text"><?php echo implode(', ',$texts['Profile Item Type Metadata']['Bibliography']); ?></div>
+                            </div>
+                            <?php endif; ?>
+
+                            <?php if (isset($texts['Profile Item Type Metadata']['Other links'])): ?>
+                            <div class="element">
+                                <h3><?php echo 'Other links'; ?></h3>
+                                <div class="element-text"><?php echo implode(', ',$texts['Profile Item Type Metadata']['Other links']); ?></div>
+                            </div>
+                            <?php endif; ?>
+
+                            <!-- social -->
+                            <div class="panel">
+                              <?php if (isset($texts['Profile Item Type Metadata']['Telephone number'])): ?>
+                              <div class="element">
+                                  <h3><i class="fa fa-phone" aria-hidden="true"></i></h3>
+                                  <div class="element-text"><?php echo implode(', ',$texts['Profile Item Type Metadata']['Telephone number']); ?></div>
+                              </div>
+                              <?php endif; ?>
+
+                              <?php if (isset($texts['Profile Item Type Metadata']['Email'])): ?>
+                              <div class="element">
+                                  <h3><i class="fa fa-envelope-o" aria-hidden="true"></i></h3>
+                                  <div class="element-text"><?php echo implode(', ',$texts['Profile Item Type Metadata']['Email']); ?></div>
+                              </div>
+                              <?php endif; ?>
+                              <?php if (isset($texts['Profile Item Type Metadata']['LinkedIn'])): ?>
+                                <div class="element">
+                                    <h3><i class="fa fa-linkedin-square" aria-hidden="true"></i></h3>
+                                    <div class="element-text"><?php echo $texts['Profile Item Type Metadata']['LinkedIn'][0]; ?></div>
+                                </div>
+                              <?php endif; ?>
+                              <?php if (isset($texts['Profile Item Type Metadata']['Twitter'])): ?>
+                                <div class="element">
+                                    <h3><i class="fa fa-twitter" aria-hidden="true"></i></h3>
+                                    <div class="element-text"><?php echo $texts['Profile Item Type Metadata']['Twitter'][0]; ?></div>
+                                </div>
+                              <?php endif; ?>
+                              <?php if (isset($texts['Profile Item Type Metadata']['Facebook'])): ?>
+                                <div class="element">
+                                    <h3>  <i class="fa fa-facebook-official" aria-hidden="true"></i></h3>
+                                    <div class="element-text"><?php echo $texts['Profile Item Type Metadata']['Facebook'][0]; ?></div>
+                                </div>
+                              <?php endif; ?>
+                              <?php if (isset($texts['Profile Item Type Metadata']['SlideShare'])): ?>
+                                <div class="element">
+                                    <h3><i class="fa fa-slideshare" aria-hidden="true"></i></h3>
+                                    <div class="element-text"><?php echo $texts['Profile Item Type Metadata']['SlideShare'][0]; ?></div>
+                                </div>
+                              <?php endif; ?>
+                            </div>
+
+
+                        <?php else:?>
+                              <p class="date"><?php echo metadata('item', array('Dublin Core', 'Date')); ?></p>
+                              <p class="description"><?php echo metadata('item', array('Dublin Core', 'Description')); ?></p>
+                        <?php endif; ?>
+                    </div>
+
+                    <nav style="width:100%;">
+                    <ul class="item-pagination navigation" style="width:100%;text-align:center;">
+                        <li style="margin:5px;" id="previous-item" class="previous"><?php echo link_to_previous_item_show("&#8249; Previous"); ?></li>
+                        <li style="margin:5px;" id="next-item" class="next"><?php echo link_to_next_item_show('Next &#8250;'); ?></li>
+                    </ul>
+                    </nav>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </section>
+    </div>
+</div>
 <?php echo foot(); ?>
